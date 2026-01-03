c1bfa435e5ab375a7094319e299be07670ce04fe
Nathan Helmig <nhelmig93@gmail.com>
2026-01-02 19:44:29 -0600

    convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-	return <h1>Hello, World!</h1>;
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+
+export default function Home() {
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {tasks?.map(({ _id, text }) => (
+        <div key={_id}>{text}</div>
+      ))}
+    </main>
+  );
 }
